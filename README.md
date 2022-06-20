7282378945fe4ea13714f83e8b723d9d452e616e
Skulrag <58217414+Skulrag@users.noreply.github.com>
2022-06-20 12:10:33 +0200

    Add en local

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # skulrag_buyable_carwash
 ESX Fivem script that implements ownables carwash
+
+https://discord.gg/sdnsdJbqZW
---
+++ locales/en.lua
@@ -0,0 +1,15 @@
+Locales['en'] = {
+	['press_wash'] = '~INPUT_CONTEXT~ to wash your vehicle for ~g~$%d',
+	['press_manage'] = '~INPUT_CONTEXT~ to manage your carwash station.',
+	['press_buy'] = '~INPUT_CONTEXT~ to buy the carwash station.',
+	['carwash_blip'] = 'Carwash station',
+	['withdraw_amount'] = 'Withdraw Amount',
+	['no_wash_needed'] = 'Your vehicle is clean.',
+	['cleaning_vehicle'] = 'Vehicle currently being cleaned... Please wait',
+	['cleaned_vehicle'] = 'Your vehicle was cleaned for ~g~$%d',
+	['not_enough_money'] = 'You don\'t have enough money: ~r~$%d',
+	['shop_proprio'] = 'Station managment',
+	['menu_isAlreadyOpened'] = 'Menu already used',
+	['invalid_amount'] = 'Invalid amount',
+	['have_withdrawn'] = 'You\'ve just withdrawed ~g~$%d'
+}
